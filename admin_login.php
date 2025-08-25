feb8f0c2db53dd325458e1752e515da4f427ff14
Naveen <naveen9222777@gmail.com>
2025-08-26 00:03:37 +0530

    mongodb test3

--- admin_login.php
+++ admin_login.php
@@ -19,8 +19,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if (password_verify($admin_password, $admin['password'])) {
             $_SESSION['admin_id'] = (string)$admin['_id']; // using MongoDB _id
             $_SESSION['admin_username'] = $admin_username;
-            echo "<script>alert('Login successful! Redirecting...');</script>";
-            header("refresh:1;url=registration_data.php");
+            echo "<script>alert('Login successful! Redirecting...');window.location.href='registration_data.php';</script>";
+            // header("refresh:1;url=registration_data.php");
         } else {
             echo "<script>alert('Invalid password!');window.location.href='admin_login_form.html';</script>";
         }
